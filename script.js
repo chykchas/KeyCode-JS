5e5ff82f35a32ae5d18f8ae8e2d817df752cc631
Nik <chykchas.mail@gmail.com>
2021-07-24 13:54:45 +0300

    попытка подрубить ввод, неудача

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-import KeyCodes from "./KeyCodes";
+"use strict";
+
+import KeyCodeList from "./KeyCodes";
+
+let area = document.getElementById("area");
+
+document.getElementById("ButtonInput").addEventListener("keydown", (event) => {
+  let keyName = event.key;
+  let keyCode = event.code;
+});
+
+area.value += keyName + "----" + keyCode;
