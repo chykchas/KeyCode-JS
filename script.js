308e57dc11e5928ba555088766190a5bb317ce6f
Nik <chykchas.mail@gmail.com>
2021-07-26 11:23:59 +0300

    рабочий вариант

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-//import KeyCodeList from "./KeyCodes";
-
 let area = document.getElementById("area");
+let cols = area.getAttribute("cols");
 let keyName;
 let keyCode;
+let i = 0;
 
-document.getElementById("ButtonInput").addEventListener("keydown", (event) => {
-  keyName = event.key;
-  keyCode = event.code;
-  console.log(event);
+document.getElementById("Input").addEventListener("keydown", (event) => {
+  key = event.key;
+  Code = event.code;
+  keyCode = event.keyCode;
+
+  area.value += "Key(name)" + " | " + "Code" + " | " + "KeyCode" + "\n";
+
+  // вывод keyName и keyCode в текстовое окно
+  area.value += "\n" + key + " | " + Code + " | " + keyCode + "\n";
+
+  // Автоматическая прокрутка до последнего "свежего" вывода
+  area.scrollTop = area.scrollHeight;
+
+  // Отрисовка оступа
+  do {
+    area.value += "-";
+    i++;
+  } while (i < cols - 1);
+  i = 0;
 });
